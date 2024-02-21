be65acbdba3fc327198c233d8b8e8318e89e6a7d
Abid <abidroid1@gmail.com>
2024-02-21 19:49:10 +0500

    Initial commit

---
+++ bin/sbbwu_session3.dart
@@ -0,0 +1,89 @@
+// source code
+
+/*
+ this is
+ our
+ first program
+ */
+
+
+// entry point of our program
+
+// function
+void main()
+{
+
+  /*print('hello');
+  print('world');
+  print( 8 );
+  print( 6 + 5 ); // numbers
+  print( '6 + 5'); // string
+
+  print( 10 - 14 );
+  */
+
+  // CTRL SHIFT / multi line comment
+  // CTRL / for single line comments
+
+  // variable declaration
+  // camel casing
+  String universityName;
+
+  // variable initialization
+  universityName = 'SBBWU Peshawar # 123';
+
+  String myOfficeAddress;
+  myOfficeAddress = 'FF # 1041, Deans Trade Center Peshawar';
+
+  String name = 'Abid';
+
+
+  // print(universityName);
+  // print(myOfficeAddress);
+  // print(name);
+  //
+  // String cnicNum = '17301-343433-3';
+  // print('My CNIC number is $cnicNum');
+  //
+  // int totalStudents;
+  // totalStudents = 50;
+  // print(totalStudents);
+  //
+  //
+  // bool feePaid = false;
+  // bool isMarried = false;
+  //
+  // bool isVaccinated = false;
+  //
+  // totalStudents = 52;
+  // print(totalStudents);
+  //
+  // // constant
+  // final double pi = 3.14;
+  //
+  // // string interpolation
+  // print('The value of pi is $pi');
+  //
+  // // optional
+  // String? laptopName;
+  // print(laptopName);
+  //
+  //
+  // double? gpa;
+  // gpa = 3.3;
+  //
+  // if( gpa == null){
+  //   print('Ye mera pehla semester ha');
+  // }else{
+  //   print('GPA $gpa');
+  // }
+
+
+    print ( 5 + 7);
+    print( 7 - 5);
+    print( 7 * 5 );
+
+    print( 7 / 5 );
+    print( 7  ~/ 5 );
+    print( 7 % 7 );
+}
